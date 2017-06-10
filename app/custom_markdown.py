3aa33481ccc11e90eaf3666e2133cf42c1886b03
Siwei Chen <me@chensiwei.space>
2017-06-10 12:42:24 +0800

    adjust the card display in pagination views

--- app/custom_markdown.py
+++ app/custom_markdown.py
@@ -7,35 +7,6 @@ import re
 from mistune import InlineLexer, Renderer, Markdown, BlockLexer
 from mistune_contrib import highlight, toc
 
-'''
-class KaTeXBlockMixin(object):
-    def enable_katex(self):
-        self.rules.blockkatex = re.compile(r'^\\\\\[(.*?)\\\\\]', re.DOTALL) # \[ ... \]
-        self.default_rules.insert(0, 'blockkatex')
-        print(self.default_rules)
-
-    def parse_blockkatex(self, m):
-        self.tokens.append({
-            'type': 'blockkatex',
-            'text': m.group(1)
-        })
-
-
-class KaTeXInlineMixin(object):
-    def enable_katexinline(self):
-        self.rules.inlinekatex = re.compile(r'^\${2}([\s\S]*?)\${2}(?!\$)')  # $tex$
-        self.default_rules.insert(3, 'inlinekatex')
-        self.rules.textsuppl = re.compile(r'^[\s\S]+?(?=\$)')
-        self.default_rules.insert(-1, 'textsuppl')
-
-    def output_inlinekatex(self, m):
-        return self.renderer.inlinekatex(m.group(1))
-
-    def output_textsuppl(self, m):
-        text = m.group(0)
-        return self.renderer.text(text)
-'''
-
 
 class KaTeXRenderer(Renderer, toc.TocMixin, highlight.HighlightMixin):
     def __init__(self, *args, **kwargs):
